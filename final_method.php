edcc7d0120dcc7576581897a57867279f60ec899
ivanfelly-hue <ivanfelly@gmail.com>
2026-04-22 18:23:01 +0700

    ketigaa

---
+++ final_method.php
@@ -0,0 +1,19 @@
+<?php
+
+class User {
+
+    final public function login() {
+        echo "Login berhasil";
+    }
+}
+
+class Admin extends User {
+
+    // ERROR jika override
+    // public function login() {}
+}
+
+$a = new Admin();
+$a->login();
+
+?>
